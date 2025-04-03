021909ab2d541d3242f657781b77c3990e51802c
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2025-04-03 14:50:44 +0900

    25.04.03 오후 수업
    공격 콤보 넣기

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -27,6 +27,12 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
 
+    [Header("공격 정보")]
+    [SerializeField] private float comboTime = 0.3f;
+    private float comboTimeCounter;
+    private bool isAttacking;
+    private int comboCounter;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -42,40 +48,88 @@ public class Player : MonoBehaviour
 
         dashTime -= Time.deltaTime;
         dashCooldownTimer -= Time.deltaTime;
-        // 쿨타임 도 양수일 때만 가능하게끔 한다.
-        if(Input.GetKeyDown(KeyCode.LeftShift) && dashCooldownTimer < 0)
-        {
-            dashCooldownTimer = dashCooldown;
-            dashTime = dashDuration;
-        }
+        comboTimeCounter -= Time.deltaTime;
 
         FlipController();
         AnimatorControllers();
 
     }
 
+    public void AttackOver()
+    {
+        isAttacking = false;
 
+        comboCounter++;
+        
+        if (comboCounter > 2)
+        {
+            comboCounter = 0;
+        }
+
+    }
     void CollisionCheck()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
     private void CheckInput()
     {
+        
+
         xInput = Input.GetAxisRaw("Horizontal");
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+
+    }   
+
+    private void StartAttackEvent()
+    {
+        // 땅에서만 공격 가능하게
+        if (!isGrounded) return;
+
+        // 제 시간안에 못 치면 콤보 초기화
+        if (comboTimeCounter < 0)
+        {
+            comboCounter = 0;
+        }
+
+        isAttacking = true;
+        comboTimeCounter = comboTime;
+    }
+    
+    private void DashAbility()
+    {
+        if(dashCooldownTimer < 0 && !isAttacking)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
     }
 
     private void Movement()
-    {
-        if (dashTime > 0)
+    {   
+        if (isAttacking)
         {
-            rb.linearVelocity = new Vector2(xInput * dashSpeed,0);
+            rb.linearVelocity = new Vector2(0, 0);
         }
-        else
+        else if (dashTime > 0) // 대시
+        {
+            rb.linearVelocity = new Vector2(facingDir * dashSpeed,0);
+        }
+        else // 그냥 이동
         {
             rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
         }
@@ -97,6 +151,8 @@ public class Player : MonoBehaviour
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
     }
 
     private void Flip()
